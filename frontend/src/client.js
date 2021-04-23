40cd5d0b894d6d260bab52e8295d0f6b4e7c3086
Ananto30 <azizulhaq.ananto@gmail.com>
2021-04-23 16:55:06 +0600

    Change api base url

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://192.168.0.107:5000/api",
-  baseURL: "/api",
+  baseURL: "https://askhadith.herokuapp.com/api",
   headers: {
     "Content-Type": "application/json",
   },

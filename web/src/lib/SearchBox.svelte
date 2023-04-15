5ce5417472a3a2eeed2b6e8a1c043cfb9fe6bc81
Azizul Haque Ananto <azizulhaq.ananto@gmail.com>
2023-04-15 11:39:33 +0200

    Fix searchkey

--- web/src/lib/SearchBox.svelte
+++ web/src/lib/SearchBox.svelte
@@ -17,7 +17,7 @@
 		$collectionsSorted = new Array();
 
 		try {
-			const response = await fetch(`https://ask-hadith.vercel.app/api/search?search=${searchKey}`);
+			const response = await fetch(`https://ask-hadith.vercel.app/api/search?search=${$searchKey}`);
 			// const response = await fetch(`http://localhost:3000/api/search?search=${$searchKey}`);
 
 			const resp = await response.json();

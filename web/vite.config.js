c7e87a33e1fed82e2a8df9129c3bc51eac2236cd
Azizul Haque Ananto <azizulhaq.ananto@gmail.com>
2026-09-13 07:59:44 +0200

    update deps

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,16 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
-import fs from 'fs';
+import fs from 'node:fs';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit(), rawFonts(['.ttf'])]
 };
 
+/** @param {string[]} ext */
 function rawFonts(ext) {
 	return {
 		name: 'vite-plugin-raw-fonts',
-		transform(code, id) {
+		transform(/** @type {string} */ code, /** @type {string} */ id) {
 			if (ext.some((e) => id.endsWith(e))) {
 				const buffer = fs.readFileSync(id);
 				return { code: `export default ${JSON.stringify(buffer)}`, map: null };
